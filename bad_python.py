3be817bdabb886c2ef3bb223581531a489038cf3
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2022-12-20 12:26:35 +0000

    added more bad python to trigger sonarcloud

--- bad_python.py
+++ bad_python.py
@@ -8,3 +8,9 @@ duplicated_block = "THIS IS DUPLICATED"
 duplicated_block = "THIS IS DUPLICATED"
 if name != None:
     print(name)
+if name != None:
+    print(name)
+if name != None:
+    print(name)
+if name != None:
+    print(name)

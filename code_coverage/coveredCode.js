c6fd687b3c597b513e76e44ef936e2106cc7f47f
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-01-04 09:59:36 +0000

    removed bad code

--- code_coverage/coveredCode.js
+++ code_coverage/coveredCode.js
@@ -5,16 +5,3 @@ export const multiplyBy5 = (number_to_multiply) => {
 export const multiplyBy10 = (number_to_multiply) => {
   return number_to_multiply * 10;
 };
-
-export const uncoveredMethod = () => {
-  return "I AM NOT COVERED BY A TEST";
-};
-export const methodThatSmells = () => {
-  let someDuplicatedVar1 = "THE SAME STRING";
-  let someDuplicatedVar2 = "THE SAME STRING";
-  let someDuplicatedVar3 = "THE SAME STRING";
-  if (false) {
-    return someDuplicatedVar1;
-  }
-  return "I SMELL";
-};

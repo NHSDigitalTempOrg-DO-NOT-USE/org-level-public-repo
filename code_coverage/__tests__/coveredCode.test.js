e4e697d1ec66b8a494790ce4f0981f23062d326e
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-01-04 09:35:48 +0000

    added more code to cover

--- code_coverage/__tests__/coveredCode.test.js
+++ code_coverage/__tests__/coveredCode.test.js
@@ -1,4 +1,4 @@
-import { multiplyBy5 } from "../coveredCode.js";
+import { multiplyBy10, multiplyBy5 } from "../coveredCode.js";
 
 describe("multiplyBy5", () => {
   it("multiplies numbers by 5", () => {
@@ -6,6 +6,16 @@ describe("multiplyBy5", () => {
     const expectedNumber = 10;
 
     const actualNumber = multiplyBy5(numberToMultiply);
+
+    expect(actualNumber).toEqual(expectedNumber);
+  });
+});
+describe("multiplyBy10", () => {
+  it("multiplies numbers by 10", () => {
+    const numberToMultiply = 2;
+    const expectedNumber = 20;
+
+    const actualNumber = multiplyBy10(numberToMultiply);
 
     expect(actualNumber).toEqual(expectedNumber);
   });

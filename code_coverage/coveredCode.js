db2063a9aa0ccc05cb89484e1dd8f1febccb642c
Amaan Ibn-Nasar <amaan.ibn-nasar1@nhs.net>
2023-01-04 09:49:02 +0000

    added more code that isn't covered

--- code_coverage/coveredCode.js
+++ code_coverage/coveredCode.js
@@ -5,3 +5,7 @@ export const multiplyBy5 = (number_to_multiply) => {
 export const multiplyBy10 = (number_to_multiply) => {
   return number_to_multiply * 10;
 };
+
+export const uncoveredMethod = () => {
+  return "I AM NOT COVERED BY A TEST";
+};
